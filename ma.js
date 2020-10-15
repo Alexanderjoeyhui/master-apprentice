dde9a99896de6f93365a034b2f85c0f3a0248792
Alexanderjoeyhui <72459952+Alexanderjoeyhui@users.noreply.github.com>
2020-10-15 19:11:34 +0100

    Add files via upload

---
+++ ma.js
@@ -0,0 +1,143 @@
+var notchStone = document.getElementById("notch-stone");
+var notchDesert = document.getElementById("notch-desert");
+var notchGreen = document.getElementById("notch-green");
+var notchBlue = document.getElementById("notch-blue");
+var notchNavy = document.getElementById("notch-navy");
+var notchBlack = document.getElementById("notch-black");
+
+var basicDesert = document.getElementById("basic-desert");
+var basicGreen = document.getElementById("basic-green");
+var bicycleNavy = document.getElementById("bicycle-navy");
+var huntingDesert = document.getElementById("hunting-desert");
+var militaryGreen = document.getElementById("military-green");
+var westernBlack = document.getElementById("western-black");
+
+var apronStone = document.getElementById("apron-stone");
+var apronDesert = document.getElementById("apron-desert");
+var apronGreen = document.getElementById("apron-green");
+var apronBlue = document.getElementById("apron-blue");
+var apronNavy = document.getElementById("apron-navy");
+var apronBlack = document.getElementById("apron-black");
+
+var toteStone = document.getElementById("tote-stone");
+var toteDesert = document.getElementById("tote-desert");
+var toteGreen = document.getElementById("tote-green");
+var toteBlue = document.getElementById("tote-blue");
+var toteNavy = document.getElementById("tote-navy");
+var toteBlack = document.getElementById("tote-black");
+
+var giantToteNavy = document.getElementById("giant-tote-navy");
+var giantToteBlack = document.getElementById("giant-tote-black");
+
+
+
+notchStone.addEventListener("click", function(){
+    window.location.href="notch-stone.html";
+});
+
+notchDesert.addEventListener("click", function(){
+    window.location.href="notch-desert.html";
+});
+
+notchGreen.addEventListener("click", function(){
+    window.location.href="notch-green.html";
+});
+
+notchBlue.addEventListener("click", function(){
+    window.location.href="notch-blue.html";
+});
+
+notchNavy.addEventListener("click", function(){
+    window.location.href="notch-navy.html";
+});
+
+notchBlack.addEventListener("click", function(){
+    window.location.href="notch-black.html";
+});
+
+
+
+basicDesert.addEventListener("click", function(){
+    window.location.href="basic-desert.html";
+});
+
+basicGreen.addEventListener("click", function(){
+    window.location.href="basic-green.html";
+});
+
+bicycleNavy.addEventListener("click", function(){
+    window.location.href="bicycle-navy.html";
+});
+
+huntingDesert.addEventListener("click", function(){
+    window.location.href="hunting-desert.html";
+});
+
+militaryGreen.addEventListener("click", function(){
+    window.location.href="military-green.html";
+});
+
+westernBlack.addEventListener("click", function(){
+    window.location.href="western-black.html";
+});
+
+
+
+apronStone.addEventListener("click", function(){
+    window.location.href="apron-stone.html";
+});
+
+apronDesert.addEventListener("click", function(){
+    window.location.href="apron-desert.html";
+});
+
+apronGreen.addEventListener("click", function(){
+    window.location.href="apron-green.html";
+});
+
+apronBlue.addEventListener("click", function(){
+    window.location.href="apron-blue.html";
+});
+
+apronNavy.addEventListener("click", function(){
+    window.location.href="apron-navy.html";
+});
+
+apronBlack.addEventListener("click", function(){
+    window.location.href="apron-black.html";
+});
+
+
+
+toteStone.addEventListener("click", function(){
+    window.location.href="tote-stone.html";
+});
+
+toteDesert.addEventListener("click", function(){
+    window.location.href="tote-desert.html";
+});
+
+toteGreen.addEventListener("click", function(){
+    window.location.href="tote-green.html";
+});
+
+toteBlue.addEventListener("click", function(){
+    window.location.href="tote-blue.html";
+});
+
+toteNavy.addEventListener("click", function(){
+    window.location.href="tote-navy.html";
+});
+
+toteBlack.addEventListener("click", function(){
+    window.location.href="tote-black.html";
+});
+
+
+giantToteNavy.addEventListener("click", function(){
+    window.location.href="giant-tote-navy.html";
+});
+
+giantToteBlack.addEventListener("click", function(){
+    window.location.href="giant-tote-black.html";
+});
